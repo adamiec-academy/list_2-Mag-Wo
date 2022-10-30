81797b2bc0f7e97b5463d6d41cc81a198384c57c
Wojciech Adamiec <56234206+WojciechAdamiec@users.noreply.github.com>
2022-10-31 00:14:14 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,5 +1,5 @@
 def is_prime(n):
-        if n < 2:
+    if n < 2:
         return False
     for i in (2,(n-1)):
         if (n % i) == 0:
@@ -12,7 +12,6 @@ def is_diabolic(n):
 
 
 def final(n):
-
     for i in range (100000):
         if is_diabolic(i) and is_prime(i):
             print(i)
