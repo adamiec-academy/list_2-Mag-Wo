14d5b630dc1d3efd41897754ab7a002644d80fff
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-10-30 19:49:48 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,6 +1,19 @@
 def is_prime(n):
-    return
+        if n < 2:
+        return False
+    for i in (2,(n-1)):
+        if (n % i) == 0:
+            return False
+    return True
 
 
 def is_diabolic(n):
-    return
+    return "666" in str(n)
+
+
+def final(n):
+
+    for i in range (100000):
+        if is_diabolic(i) and is_prime(i):
+            print(i)
+     
