b182d4511ce1af1317b14418e8a34e8a3e682c00
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-10-30 19:14:38 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,23 @@
 def remove_parentheses(text):
-    return text
+    result = ""
+    inside = False
+
+    for l in text:
+        if l == "(":
+            inside == True
+        elif l == ")":
+            inside == True
+        elif str.index(text,"(") < str.index(text,l) < str.index(text,")"):
+            inside == True
+        elif inside == True:
+            pass
+        else:
+            result += l
+
+
+    return result
+
+ text = "(Nie) jest tak źle"
+
+print(remove_parentheses(text))
+
