d368ac5c56ab9cb8223ea0fd952640fa89fcc269
Mag-Wo <115737730+Mag-Wo@users.noreply.github.com>
2022-11-01 17:01:48 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,24 @@
-# Remove this comment to confirm that this task is done
+
+from random import randint
+
+from turtle import forward, speed, left, penup, pendown, exitonclick
+
+speed("slow")
+
+infl = 40
+
+def slupek(a,b):
+    for _ in range(2):
+        forward(a)
+        left(90)
+        forward(b)
+        left(90)
+
+for _ in range (15):
+    slupek(20,infl)
+    penup()
+    forward(40)
+    pendown()
+    infl += randint (-20, 55)
+
+exitonclick()
